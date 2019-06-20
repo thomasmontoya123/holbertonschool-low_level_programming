fe945d8b23f4ee643c00cafef3a632abd494771b
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-06-20 01:42:31 +0000

    typo fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unisd.h>
 /**
- * main - This program prints the size of various types
- * on the computer it is compiled and run on.
- * Return: Always 0 (Success)
+ * main - Entry point
  *
-int main (void)
+ * Return: Always 1 (Success)
+ */
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
