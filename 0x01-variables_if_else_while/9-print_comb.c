92bf354c52efae854ca64ba3afe8b379c514bbe4
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-06-20 20:44:42 +0000

    digits follewd by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
