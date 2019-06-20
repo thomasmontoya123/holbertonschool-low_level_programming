0c06753863a472033d13c644a13d1f3b82d8e6b2
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-06-20 19:49:04 +0000

    typo fixed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 48;
+
+	while (c <= 59)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+
+
+	return (0);
+}
