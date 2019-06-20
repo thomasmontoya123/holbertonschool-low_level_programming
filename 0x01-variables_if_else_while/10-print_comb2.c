fa83b9c3878d9831b367f1a9308c02e472f23d55
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-06-20 21:56:42 +0000

    unnecesary if removed

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 00 to 99.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = '0';
+	b = '0';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+
+				putchar(a);
+				putchar(b);
+				if (a == '9' && b == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	return (0);
+}
+
