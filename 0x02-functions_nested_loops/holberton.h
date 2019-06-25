9cf6fadef55be80eac59c3c115f0a51a1810e623
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-06-25 00:24:03 +0000

    headers and functions

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,20 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet_x10(void);
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+#endif
