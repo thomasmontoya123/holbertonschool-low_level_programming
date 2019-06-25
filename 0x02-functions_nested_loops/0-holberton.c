da3e264da7a3195417e11087684a365d5f493cd4
thomas <tomasmontoya123@gmail.com>
2019-06-25 10:33:23 -0500

    tabs instead spaces

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-
 /**
- * main -  holberton
- * Return: Always 0 (Success)
+ * main -  "Holberton"
+ * Return: 0 on success.
  */
 int main(void)
 {
-char c[] = "Holberton";
+	char c[] = "Holberton";
+	int a;
 
-int a;
-
-for (a = 0; c[a] != '\0'; a++)
-{
-_putchar(c[a]);
-}
-_putchar('\n');
-return (0);
+	for (a = 0; a < 9; a++)
+	{
+		_putchar(c[a]);
+	}
+	 _putchar('\n');
+	return (0);
 }
