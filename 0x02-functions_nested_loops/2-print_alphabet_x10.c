774f420a1c0597eabe296175a489d95c38d944e8
thomas <tomasmontoya123@gmail.com>
2019-06-25 10:51:33 -0500

    main moved apart

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,10 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * print_alphabet_x10 - alpahabet ten times
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
 
 void print_alphabet_x10(void)
 {
