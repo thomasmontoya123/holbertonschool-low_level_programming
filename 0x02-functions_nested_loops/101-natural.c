7e4cf77913589f0788898cbf59e2152bc461ff69
Thomas Montoya <thomasmontoya@Jorges-MBP-2.lan>
2019-06-26 14:06:32 -0500

    sum fixed

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a = 3;
+	int sum = 0;
+
+	do {
+		if ((a % 3 == 0) || (a % 5 == 0))
+			sum = sum + i;
+
+		a++;
+	} while (a < 1024);
+
+	printf("%i\n", sum);
+	return (0);
+}
