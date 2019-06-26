452d0953b22b7c274c984f27ab20951d1e0ee170
Thomas Montoya <thomasmontoya@Jorges-MacBook-Pro-2.local>
2019-06-26 14:49:31 -0500

    logic correction

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+long sum = 0;
+
+long first = 0;
+
+long sec = 1;
+
+long a = 1;
+
+
+do {
+if (f % 2 == 0)
+
+sum = sum + a;
+
+printf("%li (%li), ", a, sum);
+first  = sec;
+
+sec = a;
+
+a = first + sec;
+
+} while (a < 4000001);
+
+printf("%li\n", sum);
+
+return (0);
+}
+
