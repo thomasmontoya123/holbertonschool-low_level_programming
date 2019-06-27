e97ccd3582ec5d3c88df713a75a4412c921776e5
Thomas Montoya <thomasmontoya@Jorges-MacBook-Pro-2.local>
2019-06-27 10:45:10 -0500

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *  * _isdigit - finds for digits
+ *   * @c: integer value for the function
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return: digitr  return 1
+ *
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+
+	return (1);
+
+else
+
+	return (0);
+
+}
