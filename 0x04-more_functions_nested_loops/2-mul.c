10f75b83d888b837c67351a76d07abe72b0bef46
Thomas Montoya <thomasmontoya@Jorges-MBP-2.lan>
2019-06-27 11:04:01 -0500

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - two integers multiplication
+ * @a: integer value for the function
+ * @b: interger value for the function
+ *
+ * Return: res
+ */
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
