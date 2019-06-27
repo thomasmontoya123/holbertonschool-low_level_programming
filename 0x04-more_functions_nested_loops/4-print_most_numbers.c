705e4a1db0e65a1cd25445d1d252a3cd7668e675
Thomas Montoya <thomasmontoya@Jorges-MacBook-Pro-2.local>
2019-06-27 12:03:56 -0500

    numbers with exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints the numbers 0 - 9 without 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+		{
+			_putchar (a);
+		}
+	}
+	_putchar('\n');
+}
