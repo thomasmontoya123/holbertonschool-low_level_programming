2c153185b81b196472a89fd9341dc66f1c74a745
Thomas Montoya <thomasmontoya@Jorges-MacBook-Pro-2.local>
2019-06-27 10:29:31 -0500

    header, readme, 0x

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *  * _isupper - finds for uppercase
+ *   * @c: integer value for the function
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return: upper  return 1
+ *
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+
+	return (1);
+
+else
+
+	return (0);
+
+}
