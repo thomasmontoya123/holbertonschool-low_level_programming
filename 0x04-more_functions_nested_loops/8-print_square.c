0c05e43a4339f74724fa9cc8ac001d86d97211ca
Thomas Montoya <thomasmontoya@Jorges-MBP-2.lan>
2019-06-27 14:04:02 -0500

    condition fixed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_square - draws a square.
+ * @size: value for the function
+ *
+ * Return: void.
+ *
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			_putchar('#');
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
