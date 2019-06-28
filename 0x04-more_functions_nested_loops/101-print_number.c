08045355edb9abee4321e3f121c8dc2f02837997
Thomas Montoya <thomasmontoya@Jorges-MacBook-Pro-2.local>
2019-06-27 21:49:04 -0500

    minimun negative fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,42 +1,31 @@
 #include "holberton.h"
 /**
-  * print_number - print numbers out
-  * @n: integer value for the function 
-  */
-void print_number(int n)
+ * print_number - integer print
+ * @num: value for function
+ * Return: return 0
+ */
+
+
+void print_number(int num)
 {
-	int a, b, d;
-	int num = n;
+unsigned int top = 0;
 
-	d = 10;
-
-	if (n < 0)
-	{
-		num = num * -1;
-		_putchar('-');
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		for (a = 1; num > 0; a++)
-		{
-			num /= 10;
-		}
-			for (b = 0; b < a - 1; b++)
-			{
-				d *= 10;
-
-
-			while (d >= 1)
-			{
-				_putchar(n / d + '0');
-				n = n % d;
-				d = d / 10;
-
-			}
-		}
-	}
+if (num % 10 < 0)
+{
+	_putchar('-');
+	top = -num;
+}
+else
+{
+	top = num;
+}
+if (top < 10)
+{
+	_putchar('0' + (top % 10));
+}
+else
+{
+		print_number(top / 10);
+	_putchar('0' + top % 10);
+}
 }
