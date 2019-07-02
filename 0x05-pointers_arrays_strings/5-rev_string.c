8f296aa590a1a97403d3568ed3e55b96b4dcbf4b
Thomas Montoya <thomasmontoya@Jorges-MacBook-Pro-2.local>
2019-07-02 10:35:02 -0500

    counter position fixed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * rev_string  - function that reverse string
+ * @s: string 
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return:void
+ *
+ */
+void rev_string(char *s)
+{
+	int a, b;
+	b = 0;
+	a = 0; 
+
+	char temp;
+
+	while (s[a] != 0)
+	{
+		a++;
+	}
+		
+
+	for (a = a - 1; b < a; a--)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+		b++;
+	}
+}
