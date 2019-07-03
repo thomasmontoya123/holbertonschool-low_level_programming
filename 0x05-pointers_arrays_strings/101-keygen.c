144f526edc365f951ebc0e557c24e970a3e380ef
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-07-03 15:09:43 -0500

    sum 2772

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - cracker.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int array[1000];
+	int pass, counter, temp;
+	srand(time(null));
+
+	for ( counter = 0; i < 500; i++)
+	{
+		array[counter] = rand % 42;
+		pass = pass + (array[counter] + '0');
+		putchar(array[counter] + '0');
+
+		if ((2772 - pass) - '0' < 42)
+		{
+			temp = 2772 - pass - '0';
+			pass = pass + temp;
+			putchar(temp + '0');
+			break;
+		}	
+		return (0);
+	}
