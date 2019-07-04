b4051c6b9ecca23d8399b467f11ab467573fd539
thomas <tomasmontoya123@gmail.com>
2019-07-04 15:59:13 -0500

    array type

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * reverse_array - function that reverse string
+ * @a: string
+ * @n: number of elemnts
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return:void
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, b;
+	int temp[500];
+
+	i = 0;
+	b = 0;
+	int c = n;
+
+	while (i < n)
+	{
+		temp[i] = a[i];
+		i++;
+	}
+
+	c--;
+
+	while (c >= 0)
+	{
+		a[c] = temp[b];
+		c--;
+		b++;
+	}
+}
