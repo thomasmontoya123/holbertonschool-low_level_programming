a354ca8414b5c87fb431cde99fbd6ebba3f01a38
thomas <tomasmontoya123@gmail.com>
2019-07-04 22:59:53 -0500

    for instead while

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * *leet - encodes into 1337
+ * @trad: translation
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return: encode
+ *
+ */
+char *leet(char *trad)
+{
+	int n;
+	int a;
+	char  letter[5][2] = {{'a', 'A'}, {'e', 'E'}, {'o', 'O'}, {'t', 'T'}, {'l', 'L'}};
+	char  numtable[] = {'4', '3', '0', '7', '1'};
+
+	for (a = 0; trad[a] != '\0'; a++)
+	{
+		for (n = 0; n < 5; n++)
+		{
+			if (letter[n][0] == trad[a] || letter[n][1] == trad[a])
+			{
+				trad[a] = numtable[n];
+			}
+
+		}
+
+	}
+	return (trad);
+}
