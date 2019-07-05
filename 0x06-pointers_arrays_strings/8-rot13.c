769ec2ac0d280585f1b027c466e75c03b38336c4
thomas <tomasmontoya123@gmail.com>
2019-07-04 23:19:02 -0500

    for instead of while

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * root13 - encodes into root13
+ * @cypher: translation
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return: encode
+ *
+ */
+char *rot13(char *cypher)
+{
+	int a;
+	int b;
+	char in[] ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char trans[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+
+	for (a = 0; cypher[a] != '\0'; a++)
+	{
+		for (b = 0; b <= 52; b++)
+		{
+			if (cypher[a] == in[b])
+			{
+				cypher[a] = trans[b];
+				break;
+			}
+		}
+	}
+	return (cypher);
+}
