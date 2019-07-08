f63bf7201488e7b7a805ffe3e55900bf6f98d462
thomas <tomasmontoya123@gmail.com>
2019-07-08 15:59:05 -0500

    betty fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
 /**
  * _strspn - lenght of a prefix substring
  * @s: string
- * @c: character
+ * @accept: string for comp
  *
  * Return: dest
  *
@@ -10,14 +10,14 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
-	int acceptsize ;
+	int acceptsize;
 	int repeats = 0;
 
 	for (acceptsize = 0; accept[acceptsize] != 0; acceptsize++)
 	{
 		;
 	}
-	for ( i = 0;  i <= acceptsize ; i++)
+	for (i = 0;  i <= acceptsize ; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
