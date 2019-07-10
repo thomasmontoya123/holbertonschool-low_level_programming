6ef32c0ba3086f97a079592fec2ce403e835bce4
thomas <tomasmontoya123@gmail.com>
2019-07-10 11:18:07 -0500

    prefix ++s

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - print string
+ * @s: string
+ * Return: void
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion(++s);
+
+}
