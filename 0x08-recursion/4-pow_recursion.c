2010365c7f6516ebfd108c4a3ca0af3429fe881f
thomas <tomasmontoya123@gmail.com>
2019-07-10 13:16:24 -0500

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - x raised to the power of y
+ * @x: base
+ * @y: power
+ * Return: strlength
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (x *  _pow_recursion(x, y - 1));
+	}
+}
