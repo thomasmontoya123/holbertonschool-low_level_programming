2b808b87fb67a511c8ac79544cd1895ebd7ddd39
thomas <tomasmontoya123@gmail.com>
2019-07-11 20:56:15 -0500

    strlen added

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,4 +1,19 @@
 #include "holberton.h"
+/**
+ * _strlen_recursion - print string in reverse
+ * @s: string
+ * Return: strlength
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+
+}
 
 /**
  * comparison - compare for palindromes
