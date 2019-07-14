f7192c608b77ac457f56c112e86f894550242a91
thomas <tomasmontoya123@gmail.com>
2019-07-14 13:54:00 -0500

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * return: 0
  */
 
-int main (int argc, char * argv[])
+int main(int argc, char *argv[])
 {
- 	(void) argc;
-	printf ("%s\n", argv[0]);
-	return(0);
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
