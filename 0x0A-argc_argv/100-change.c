271f7cd2212c85232375587d007769007dcbc07f
thomas <tomasmontoya123@gmail.com>
2019-07-15 18:06:08 -0500

    spaces

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -60,10 +60,10 @@ int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		printf("Error \n");
+		printf("Error\n");
 		return (1);
 	}
 
-	printf("%i \n", changes(argv[1]));
+	printf("%i\n", changes(argv[1]));
 	return (0);
 }
