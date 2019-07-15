ba0cb942a3171566a18c070bd5a30e7816cd0262
thomas <tomasmontoya123@gmail.com>
2019-07-15 17:42:12 -0500

    first add coints, then changes menuda

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * changes - get the amoount of coins
+ * @money: money
+ *
+ * Return: coins
+ *
+ */
+
+int changes(char *money)
+{
+	int menuda = atoi(money);
+	int coins = 0;
+
+		if (menuda / 25 > 0)
+		{
+			coins += menuda / 25;
+			menuda %= 25;
+		}
+		if ((menuda / 10) > 0)
+		{
+			coins += menuda / 10;
+			menuda %= 10;
+		}
+		if ((menuda / 5) > 0)
+		{
+			coins += menuda / 5;
+			menuda %= 5;
+		}
+		if ((menuda / 2) > 0)
+		{
+			coins += menuda / 2;
+			menuda %= 2;
+		}
+		else
+		{
+			coins++;
+			menuda--;
+		}
+
+
+		return (coins);
+}
+/**
+ * main - Entry point
+ * @argc: number of commands
+ * @argv: string of commands
+ *
+ * Return: int
+ *
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error \n");
+		return (1);
+	}
+
+	printf("%i \n", changes(argv[1]));
+	return (0);
+}
