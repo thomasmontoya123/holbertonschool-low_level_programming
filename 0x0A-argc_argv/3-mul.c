9d34e7cb185a836e8f87b9aedf4d763a21bd9ba0
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-07-14 19:24:18 -0500

    if and else swap

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if (argc != 3)
-	{
-		printf("Error \n");
-		return (1);
-	}
-
-	else
+	if (argc == 3)
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d \n", result);
 	}
 
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 
 }
