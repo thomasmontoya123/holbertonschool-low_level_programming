2466a88ebd81676bfb0b9a29eb647662c1f01f61
thomas <tomasmontoya123@gmail.com>
2019-07-15 17:51:28 -0500

    else rm

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,31 +13,29 @@ int changes(char *money)
 	int menuda = atoi(money);
 	int coins = 0;
 
-		if (menuda / 25 > 0)
+		if (menuda / 25 >= 0)
 		{
 			coins += menuda / 25;
 			menuda %= 25;
 		}
-		if ((menuda / 10) > 0)
+		if ((menuda / 10) >= 0)
 		{
 			coins += menuda / 10;
 			menuda %= 10;
 		}
-		if ((menuda / 5) > 0)
+		if ((menuda / 5) >= 0)
 		{
 			coins += menuda / 5;
 			menuda %= 5;
 		}
-		if ((menuda / 2) > 0)
+		if ((menuda / 2) >= 0)
 		{
 			coins += menuda / 2;
 			menuda %= 2;
 		}
-		else
-		{
-			coins++;
-			menuda--;
-		}
+
+		coins += menuda / 1;
+		menuda %= 1;
 
 
 		return (coins);
