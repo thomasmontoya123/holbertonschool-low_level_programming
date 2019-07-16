441e7a1f814ddd38fb5c8b576bf32af4ed6e385c
thomas <tomasmontoya123@gmail.com>
2019-07-16 17:15:09 -0500

    free fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,6 +14,7 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j = 0;
+	int a;
 	int **gr;
 
 	if (width <= 0 || height <= 0)
@@ -21,22 +22,21 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	gr = (int **)malloc(height * sizeof(int *));
-
 	if (gr == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < height; i++)
 	{
 		gr[i] = (int *)malloc(width * sizeof(int));
 		if (gr[i] == NULL)
 		{
-			for (j = i; j <= 0; j--)
-			{
-				free(gr[j]);
-			}
 			free(gr);
+
+			for (a = 0; a < i; a++)
+			{
+				free(gr[a]);
+			}
 			return (NULL);
 		}
 	}
