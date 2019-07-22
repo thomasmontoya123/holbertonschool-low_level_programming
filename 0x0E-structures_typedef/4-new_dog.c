4220e2679d1dd923d77babafbc6179e22b3b2801
thomas <tomasmontoya123@gmail.com>
2019-07-22 16:06:43 -0500

    creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strcpy - function that prints n elements
+ * @src: array source
+ * @dest: array destination
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ * Return: char pointer
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != 0; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
+/**
  * new_dog - creates a new dog
  *
  * @name: dog name
@@ -15,9 +37,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
 	unsigned int namesize, ownersize, i;
-	char *cpname, *cpowner;
 
-	for (i = 0; name[i] != '\0' ; i++)
+	for (i = 0; name[i] != '\0'; i++)
 	{
 		namesize++;
 	}
@@ -27,34 +48,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
-		return (NULL);
-	cpname = malloc(sizeof(char) * namesize + 1);
-	if (cpname == NULL)
 	{
-		free(cpname);
 		free(ndog);
 		return (NULL);
 	}
-	for (i = 0; name[i] != '\0'; i++)
-	{
-		cpname[i] = name[i];
-	}
-	cpname[i] = '\0';
-	cpowner = malloc(sizeof(char) * ownersize + 1);
-	if (cpowner == NULL)
-	{
-		free(cpowner);
-		free(cpname);
-		free(ndog);
-		return (NULL);
-	}
-	for (i = 0; owner[i] != '\0'; i++)
-	{
-		cpowner[i] = owner[i];
-	}
-	cpowner[i] = '\0';
-	ndog->name = cpname;
 	ndog->age = age;
-	ndog->owner = cpowner;
+	ndog->name = malloc(sizeof(char) * namesize + 1);
+	ndog->name = _strcpy(ndog->name, name);
+	if (ndog->name == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	ndog->owner = malloc(sizeof(char) * ownersize + 1);
+	ndog->owner = _strcpy(ndog->owner, owner);
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog->owner);
+		free(ndog);
+		return (NULL);
+	}
 	return (ndog);
+
 }
