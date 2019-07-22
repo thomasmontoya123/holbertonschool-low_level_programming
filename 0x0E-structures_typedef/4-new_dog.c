512ed1e973fed6b906cc1a1d2bbb1c9142e0cc26
thomas <tomasmontoya123@gmail.com>
2019-07-22 18:29:34 -0500

    aux function removed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,28 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strcpy - function that prints n elements
- * @src: array source
- * @dest: array destination
- *   (* a blank line
- *    * Description: Longer description of the function)?
- *    (* section header: Section description)*
- * Return: char pointer
- *
- */
-char *_strcpy(char *dest, char *src)
-{
-	int a;
-
-	for (a = 0; src[a] != 0; a++)
-	{
-		dest[a] = src[a];
-	}
-
-	dest[a] = '\0';
-	return (dest);
-}
-/**
  * new_dog - creates a new dog
  *
  * @name: dog name
@@ -40,18 +18,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ndog;
 
 	for (i = 0; name[i] != '\0'; i++)
-	{
 		namesize++;
-	}
 	for (i = 0; owner[i] != '\0'; i++)
-	{
 		ownersize++;
-	}
 	ndog = malloc(sizeof(struct dog));
 	if (ndog == NULL)
-	{
 		return (NULL);
-	}
 	cpname = malloc(sizeof(char) * (namesize + 1));
 	if (cpname == NULL)
 	{
@@ -67,8 +39,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(cpname);
 		return (NULL);
 	}
-	cpname = _strcpy(cpname, name);
-	cpowner = _strcpy(cpowner, owner);
+	for (i = 0; i < namesize; i++)
+		cpname[i] = name[i];
+	for (i = 0; i < ownersize; i++)
+		cpowner[i] = owner[i];
 	ndog->name = cpname;
 	ndog->age = age;
 	ndog->owner = cpowner;
