60d2226e7b48a8c26b765b868a9a2ba4584f9aea
thomas <tomasmontoya123@gmail.com>
2019-07-22 15:31:30 -0500

    free in case fail

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,27 +27,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
-	{
 		return (NULL);
-	}
 	cpname = malloc(sizeof(char) * namesize + 1);
 	if (cpname == NULL)
 	{
+		free(cpname);
+		free(ndog);
 		return (NULL);
 	}
 	for (i = 0; name[i] != '\0'; i++)
 	{
 		cpname[i] = name[i];
 	}
+	cpname[i] = '\0';
 	cpowner = malloc(sizeof(char) * ownersize + 1);
 	if (cpowner == NULL)
 	{
+		free(cpowner);
+		free(cpname);
+		free(ndog);
 		return (NULL);
 	}
 	for (i = 0; owner[i] != '\0'; i++)
 	{
 		cpowner[i] = owner[i];
 	}
+	cpowner[i] = '\0';
 	ndog->name = cpname;
 	ndog->age = age;
 	ndog->owner = cpowner;
