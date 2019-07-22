7a764d28f5ac246167a33577e3fdf813e005acdb
thomas <tomasmontoya123@gmail.com>
2019-07-22 14:01:05 -0500

    comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,11 @@ int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
-
+/**
+ * struc dog - Typedef
+ */
 typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
