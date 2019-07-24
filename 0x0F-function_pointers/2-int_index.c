58dc1ffd970f1e6f853efa9fe11be6bb71fdcf29
thomas <tomasmontoya123@gmail.com>
2019-07-24 15:43:04 -0500

    search for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,13 +16,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	}
 
-	for (i = 0; i < size; i++)
+	if(array && cmp)
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
-		}
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 
+		}
 	}
 	return (-1);
 }
