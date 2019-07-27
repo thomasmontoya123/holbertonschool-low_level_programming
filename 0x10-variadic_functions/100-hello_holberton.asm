24aad2bd1b5908b62ce62e093410c65d768fa09d
thomas <tomasmontoya123@gmail.com>
2019-07-27 18:51:18 -0500

    assambly hello

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,16 @@
+section .data
+
+msg db "Hello, Holberton", 0x0a ; 0x0a means new line inascii
+.size equ $ - msg
+
 section .text
-
-_start:
-  mov rax, 1        
-  mov rdi, 1        
-  mov rsi, msg      
-  mov rdx, msglen   
-  syscall           
-
-  mov rax, 60       
-  mov rdi, 0        
-  syscall           
-
-section .rodata
-  msg: db "Hello, Holberton", 10
-  msglen: equ $ - msg
+	global main
+	main:
+		mov rax, 1		; call for write
+		mov rdi, rdi		; standard out
+		mov rsi, msg		; msg address
+		mov rdx, msg.size	; size of msg
+		syscall			; kernel invocation for write
+		mov rax, 60		; exit call
+		mov rdi, 0		; exit code
+		syscall			; invoke for exit
