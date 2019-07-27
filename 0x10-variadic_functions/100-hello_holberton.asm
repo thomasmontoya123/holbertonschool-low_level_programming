9b91b4beac5f471522da4911449ce5bc428f83da
thomas <tomasmontoya123@gmail.com>
2019-07-27 18:39:20 -0500

    assambly message

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .text
+
+_start:
+  mov rax, 1        
+  mov rdi, 1        
+  mov rsi, msg      
+  mov rdx, msglen   
+  syscall           
+
+  mov rax, 60       
+  mov rdi, 0        
+  syscall           
+
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg
