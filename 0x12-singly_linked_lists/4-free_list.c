701cf1ed299cb0d3cff5cc88d644460ebbde486e
thomas <tomasmontoya123@gmail.com>
2019-08-01 17:43:41 -0500

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,16 @@
 #include "lists.h"
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
 /**
  * free_list - frees list
  * @head: first node
  * Return: void
  */
+
 void free_list(list_t *head)
 {
 	list_t *temporal;
 
-	temporal = head;
-	while (temporal)
+	while (head)
 	{
 		temporal = head;
 		head = head->next;
