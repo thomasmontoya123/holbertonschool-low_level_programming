3378f89cb5b6ae00fcb64cd832daa96c078b33bf
thomas <tomasmontoya123@gmail.com>
2019-08-01 14:35:59 -0500

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,19 +13,22 @@
 
 size_t print_list(const list_t *h)
 {
-	unsigned int i;
+	size_t nodeCount = 0;
 
 	while (h)
 	{
-		if(!h->str)
+		if (!h->str)
 		{
 			printf("[0] (nil)\n");
 		}
-		printf("[%d] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 
 		h = h->next;
-		i++;
+		nodeCount++;
 
 	}
-	return (i);
+	return (nodeCount);
 }
