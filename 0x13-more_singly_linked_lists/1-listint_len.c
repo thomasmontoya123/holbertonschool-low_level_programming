c7acbc2a3d152e11559d2c4faca1ba36151fe2c8
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-06 09:52:54 -0500

    number of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * listint_len - Return number of elements
+ * @h: list
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodeCounter = 0;
+
+	while    (h)      
+	{
+		h = h->next;
+		nodeCounter++;
+	}
+
+	return (nodeCounter);
+}
