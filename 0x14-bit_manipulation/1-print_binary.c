5f4268bbec5cb356dac21c37decac7aff947f890
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-08 15:07:11 -0500

    convert int to binary and print it

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_binary - print binary in decimal
+ * @n: binary number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int bit;
+	int  i, j;
+
+	i = 0;
+
+	if (!n)
+		return;
+
+	if (n == 0)
+		_putchar ('0');
+
+	while (n >> i)
+		i++;
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		bit = n >> j;
+
+		if (bit  & 1)
+			_putchar('1');
+
+		else
+			_putchar('0');
+
+	}
+
+}
