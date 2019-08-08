ae50f256db12077b840e22662f44c01da2bf274e
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-08 18:12:46 -0500

    bits needed for  a flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - bits you would need to flip to get from one number to another
+ * @n: number
+ * @m: to flip
+ * Return: number of bits
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flipCounter = 0;
+	unsigned long int flip;
+	unsigned int max = 0;
+
+	flip = n ^ m;
+
+	while (max < 64)
+	{
+		if ((flip & 1) != 0)
+			flipCounter++;
+
+
+		flip = flip >> 1;
+
+		max++;
+	}
+
+	return (flipCounter);
+}
+
