b966ee350c9fbdad34629674c80881ed480b421f
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-08 13:39:05 -0500

    Converts binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,65 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - 2 raised to the power of y
+ * @len: power
+ * Return: power
+ *
+ */
+
+int _pow_recursion(int len)
+{
+	if (len < 0)
+	{
+		return (-1);
+	}
+
+	else if (len == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (2 *  _pow_recursion(len - 1));
+	}
+}
+
+/**
+ * binary_to_uint - conervert binary to unsigned int
+ * @b: binary string
+ * Return: power
+ *
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, len;
+	unsigned int conversed = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+		{
+			conversed += 0;
+			len--;
+		}
+
+		else if (b[i] == '1')
+		{
+			len--;
+			conversed += _pow_recursion(len);
+		}
+		else
+		{
+			return (0);
+		}
+
+	}
+	return (conversed);
+
+
+}
