5358d4b604f211fd560ea3112bd01d275484ae0a
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-13 14:43:27 -0500

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file.
+ *
+ * @filename: name of the file
+ * @text_content: text for the file.
+ *
+ * Return: 1 on succes -1 if it fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int descriptorFlag, size, writeFlag;
+
+	if (!filename)
+		return (-1);
+
+	descriptorFlag = open (filename, O_CREAT | O_WRONLY | O_TRUNC, 0600); /*0600 mean for rw-------*/
+	if (descriptorFlag == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close (descriptorFlag);
+		return (1);
+	}
+
+	for (size = 0; text_content[size]; size++)
+		;
+
+	writeFlag =  write (descriptorFlag, text_content, size);
+
+	if (writeFlag == -1)
+	{
+		write (1, "fails", 5);
+		return (-1);
+	}
+
+	return (1);
+
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,8 +1,9 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 #include <unistd.h>
-int _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
 
 #endif
