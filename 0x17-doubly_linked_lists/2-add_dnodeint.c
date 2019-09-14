7deda0247a774f1bf95222e3c9a5aba428956707
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-09-14 18:13:38 -0500

    add node at front

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,19 +9,24 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new_node;
 
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
+	if (!head)
 		return (NULL);
 
-	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-	if (*head != NULL)
-		(*head)->prev = new;
-	*head = new;
+	new_node = malloc(sizeof(dlistint_t));
 
-	return (new);
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
 }
