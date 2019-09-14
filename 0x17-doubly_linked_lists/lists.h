6607062783deaa81088e25dd06ffc95bf633e65b
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-09-14 18:54:30 -0500

    delete at a given index

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,5 +29,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /* _LISTS_H_ */
