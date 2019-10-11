4e1139a919b0731018e323bfbcc98ab70bfe1ace
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-11 14:02:47 -0500

    prints a hash table.

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - retrieves a value associated with a key.
+ * @ht: hash table to print
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i, print_counter = 0;
+	hash_node_t *temporal = NULL;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (!ht->array[i])
+			continue;
+		if (print_counter > 0)
+			printf(", ");
+
+		for (temporal = ht->array[i]; temporal; temporal = temporal->next)
+		{
+			printf("'%s:' '%s'", temporal->key, temporal->value);
+			if (temporal->next)
+				printf(" ,");
+			print_counter++;
+		}
+	}
+	printf("}\n");
+}
