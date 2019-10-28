ab11015356f054eff5d1be33d4659f2d90969024
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-28 11:17:11 -0500

    quick sort

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,91 @@
+#include "sort.h"
+
+/**
+ * swaper - swaps position values
+ * @array: array
+ * @size: size of the arry
+ * @i: value to swap
+ * @j: value to swap
+ * Return: void
+ */
+
+void swaper(int array[], int i, int j, size_t size)
+{
+	int holder = 0;
+
+	holder = array[i];
+	array[i] = array[j];
+	array[j] = holder;
+	print_array(array, size);
+}
+
+/**
+ * lomuto - Lomuto partition scheme
+ * @array: array
+ * @low: left compare
+ * @hight: right compare
+ * @size: size of the arry
+ * Return: position of the partition
+ */
+
+int lomuto(int array[], int low, int hight, int size)
+{
+	int pivot, i, j;
+
+	pivot = array[hight];
+	i = low - 1;
+
+	for (j = low; j <= hight - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swaper(array, i, j, size);
+		}
+	}
+	swaper(array, i + 1, hight, size);
+	return (i + 1);
+
+
+}
+
+/**
+ * quick_sorter - quick sort algorithm recursion
+ * @array: array
+ * @low: left compare
+ * @hight: right compare
+ * @size: size of the arry
+ * Return: Nothing
+ */
+
+void quick_sorter(int *array, int low, int hight, size_t size)
+{
+	int partition_index, left, right;
+
+	partition_index = 0;
+	left = 0;
+	right = 0;
+
+	if (low < hight)
+	{
+		partition_index = lomuto(array, low, hight, size);
+		left = partition_index - 1;
+		right = partition_index + 1;
+		quick_sorter(array, low, left, size);
+		quick_sorter(array, right, hight, size);
+	}
+}
+
+/**
+ * quick_sort - quick sort algorithm
+ * @array: array
+ * @size: size of the arry
+ * Return: Void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	quick_sorter(array, 0, size - 1, size);
+}
