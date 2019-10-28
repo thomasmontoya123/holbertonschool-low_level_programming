59484b57db8d527eeee57d27f75a4139eb168d2a
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-28 11:39:05 -0500

    bubble sort

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -10,23 +10,23 @@
 void bubble_sort(int *array, size_t size)
 {
 	int holder = 0;
-	unsigned int i;
+	unsigned int i, j;
 
 	if (!array || size < 2)
 		return;
 
-	for (i = 0; array[i] ; i++)
+	for (i = 0; (i < size - 1); i++)
 	{
-		if (i == size - 1)
-			break;
-
-		else if (array[i] > array[i + 1])
+		j = 0;
+		for (j = 0; (j < size - 1); j++)
 		{
-			holder = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = holder;
-			print_array(array, size);
-			i = -1;
+			if (array[j] > array[j + 1])
+			{
+				holder = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = holder;
+				print_array(array, size);
+			}
 		}
 	}
 }
