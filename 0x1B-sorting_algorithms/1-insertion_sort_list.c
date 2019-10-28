8103288bea2a64f967c34a51f901b0c659567d44
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-28 11:05:34 -0500

    insertion sort

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Insertion sort algorithm
+ * @list: List to sort
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temporal = NULL;
+
+	temporal = *list;
+
+	if (!list || !*list)
+		return;
+
+
+	temporal = temporal->next;
+
+
+	for (; temporal; temporal = temporal->next)
+	{
+		while ((temporal->prev) && temporal->prev->n > temporal->n)
+		{
+			temporal->prev->next = temporal->next;
+			if (temporal->next)
+				temporal->next->prev = temporal->prev;
+			temporal->next = temporal->prev;
+			temporal->prev = temporal->prev->prev;
+			temporal->next->prev = temporal;
+
+			if (temporal->prev == NULL)
+				*list = temporal;
+			else
+				temporal->prev->next = temporal;
+
+			print_list(*list);
+		}
+	}
+}
