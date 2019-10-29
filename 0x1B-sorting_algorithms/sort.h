e6017f298506ecf33d527254fa71115583e81ed0
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-29 13:14:18 -0500

    header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -25,6 +25,7 @@ void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 
 #endif
