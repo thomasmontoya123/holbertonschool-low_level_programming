5a8899a10b0c7d55084bc0b04acb2607ef2a6369
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-03 18:27:52 -0500

    Task 6

---
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ *  binary_tree_preorder - preorder traversal
+ *	@tree : root node
+ * @func : function to use in the node
+ * Return: Void
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
