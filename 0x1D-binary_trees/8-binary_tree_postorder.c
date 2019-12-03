caf66d8a6bf886b19406a8f6f7d8a0ae77971454
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-03 18:28:10 -0500

    Task 8

---
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - postorder traversal
+ * @tree : root node
+ * @func : function to use in the node
+ * Return: Void
+ */
+
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
