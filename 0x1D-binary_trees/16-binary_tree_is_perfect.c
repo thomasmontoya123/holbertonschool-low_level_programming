b67d1b11ec676d47f82d647131014459b0303b00
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-04 14:08:02 -0500

    recursion

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -89,5 +89,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
 		return (1);
 
+	binary_tree_is_perfect(tree->left);
+	binary_tree_is_perfect(tree->right);
+
 	return (0);
 }
