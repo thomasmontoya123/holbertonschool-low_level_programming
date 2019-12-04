503b743f868276a8dce5f390a47002d6765c2b6a
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-04 14:30:45 -0500

    sibling function inside

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_sibling - find a sibling
+ * @node : node
+ * Return:  pointer to the sibling node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+	if (!node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
+
+
+/**
  * binary_tree_uncle - finds the uncle
  * @node : node
  * Return:  pointer to the uncle node
