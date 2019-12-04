72cab44499856fc6610de122e58ed9a170efb321
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-04 14:28:11 -0500

    modulo fixed

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -87,8 +87,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
-		if (binary_tree_is_perfect(tree->left) +
-		binary_tree_is_perfect(tree->right) % 2 == 0)
+		if ((binary_tree_is_perfect(tree->left) +
+		binary_tree_is_perfect(tree->right)) % 2 == 0)
 			return (1);
 
 	return (0);
